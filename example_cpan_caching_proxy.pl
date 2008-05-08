acdba0212992c3af031d6228b2704480e953cb6f
Paul Miller <jettero@voltar.org>
2008-05-08 09:16:44 -0400

    typos and things

--- example_cpan_caching_proxy.pl
+++ example_cpan_caching_proxy.pl
@@ -7,7 +7,7 @@ use CPAN::CachingProxy;
 use CGI;
 use CGI::Carp qw(fatalsToBrowser);
 
-die "please use 'wget -O MIRRORED.BY http://www.cpan.org/MIRRORED.BY | grep dst_http' to select a proxy";
+die "please use 'wget -qO - http://www.cpan.org/MIRRORED.BY | grep dst_http | less' to select a proxy";
 
 my $cache = CPAN::CachingProxy->new(mirrors=>['http://www.perl.com/CPAN/']);
    $cache->run;
